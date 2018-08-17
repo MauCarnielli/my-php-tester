a3ba24adc23ba49d726c69e80515d6af0b99652e
MauCarnielli <mau.carnielli@gmail.com>
2018-08-17 11:10:50 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	$method = $_SERVER['REQUEST_METHOD'];
 
 	if($method == 'POST'){
-		$requestBody = file('https://passreset-sap-robot.herokuapp.com/');
+		$requestBody = file('php://input');
 		//$json = json_decode($requestBody);
 		echo $requestBody;
 	}else{
