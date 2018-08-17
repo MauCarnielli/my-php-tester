514e7958498d69c066ee129efcc3f4e6ad421dcd
MauCarnielli <mau.carnielli@gmail.com>
2018-08-17 10:17:40 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 
 	if($method == 'POST'){
 		echo "Deu certo esse outro POST!";
+		$requestBody = file_get_contents('php://input');
+		$json = json_decode($requestBody);
+		$id = $json->id;
+		echo "Deu certo. O ID é -> ".$id;
 	}else{
 		echo "huuummmm.. Não deu.";
 	}	
