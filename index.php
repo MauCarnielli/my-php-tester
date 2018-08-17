aaa8e6b66742fd3b0d8323fdbd97b03a85a744c3
MauCarnielli <mau.carnielli@gmail.com>
2018-08-17 10:49:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
 	if($method == 'POST'){
 		$requestBody = file('https://passreset-sap-robot.herokuapp.com/');
-		$json = json_decode($requestBody);
-		echo $json;
+		//$json = json_decode($requestBody);
+		echo $requestBody;
 	}else{
 		echo "huuummmm.. Não deu.";
 	}	
