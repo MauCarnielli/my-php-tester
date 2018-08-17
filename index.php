6553d533f664d9022ef4b80d1d6d2b0587e9126c
MauCarnielli <mau.carnielli@gmail.com>
2018-08-17 14:25:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	if($method == 'POST'){
 		$requestBody = file('php://input');
 		//$json = json_decode($requestBody);
-		var_dump($requestBody["content"]["id"]);
+		var_dump($requestBody);
 	}else{
 		echo "huuummmm.. Não deu.";
 	}	
