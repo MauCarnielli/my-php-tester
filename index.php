48cf566e342018be4c9cd71d33a148053fc55e4d
MauCarnielli <mau.carnielli@gmail.com>
2018-08-20 13:15:18 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 	$method = $_SERVER['REQUEST_METHOD'];
 
 	if($method == 'POST'){
-		$requestBody = file('php://input');
+		$requestBody = file_get_contents('php://input');
 		$json = json_decode(var_dump($requestBody[0]));
-		echo 'Segundo post deu certo';
-		var_dump($json);
+		echo 'Segundo post deu certo -> '.var_dump($requestBody[0]);
+		echo 'Será? -> '.$json;
 	}else{
 		echo "huuummmm.. Não deu.";
 	}	
